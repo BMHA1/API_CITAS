9f9f25822a4b65dbbd5eb25880d93293770f7ab5
BMHA1 <86425408+BMHA1@users.noreply.github.com>
2021-11-05 01:05:14 +0100

    funcion buscar todos los usarios, modificar por apellidos y eliminar por ID terminada

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,12 @@
+// const { where } = require('sequelize/types')
 const { User, Appointment } = require('../models/index.js')
+const { Op } = require("sequelize")
+// const { Module } = require('module')
+// const { Json } = require('sequelize/types/lib/utils')
 
 
+
+//creamos usuario
 module.exports.createUser = async (req, res) => {
 
     try {
@@ -12,10 +18,55 @@ module.exports.createUser = async (req, res) => {
         res.status(200).json({ user: newUser });
 
     } catch (error) {
-        return res.status(400).send({
+        res.status(400).send({
             message: 'No se ha podido generar un nuevo usuario.',
             errors: error,
             status: 400
         });
     }
 }
+//buscamos Usuario
+module.exports.searchUser = (req, res) => {
+
+    User.findByPk(req.params.id)
+        .then((user) => {
+            if (!user) res.status(200).send('El usuario no existe')
+            res.status(200).json({ data: user })
+        }, (error) => { res.status(400).send(error) })
+}
+//Buscamos todos los usarios
+module.exports.searchAll = (req, res) => {
+    User.findAll({})
+        .then((users) => res.status(200).json({ Data: users }),
+            (error) => { res.status(400), send(error) })
+}
+//Modificación del apellido
+module.exports.updateContent = (req, res) => {
+    let modification = req.body
+    // let clave=req.params.put
+    User.update(modification, {
+        where: {
+            lastaname: null
+        }
+    })
+        .then((modification) => res.status(200).json({ Data: modification }),
+            (error) => { res.status(200), send(error) })
+}
+
+
+//Eliminar un usuario por su ID
+module.exports.deleteUser = (req, res) => {
+    console.log(res.query.id)
+    let arr= Json.parse(res.query.id)
+    User.destroy({
+        where: {
+
+            id:{
+                [Op.in]: arr
+            }
+
+        }
+    })
+}
+
+
--- routers/user.js
+++ routers/user.js
@@ -3,8 +3,8 @@ const contoller=require('../controllers/user')
 
 
 router.post('/', contoller.createUser) // creamos usuario
-// router.get('/:id', contoller.searchUser)
-// router.post('/', contoller.login)
-
-
+router.get('/all',contoller.searchAll)//busca todos los usario
+router.get('/:id', contoller.searchUser)//busca por id
+router.put('/:put', contoller.updateContent)//modificar apellidos
+router.delete('/', contoller.deleteUser)//modificar apellidos
 module.exports=router;
