2edc231186c2d894a59a48ecaa1c0c4b079f369b
Raul <raaul9212@gmail.com>
2021-11-08 15:55:52 +0100

    Creamos Has

---
+++ Middleware/functions.js
@@ -0,0 +1,43 @@
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const moment = require("moment");
+const { User, Appointment } = require('../models/index.js')
+//generamos el hash para guardar la contraseña encriptada!
+module.exports.createHash = (password) => {
+    let encrypted = bcrypt.hashSync(password, 10)
+    return encrypted
+}
+//function para comparar el hash para guardar la contraseña encriptada!
+module.exports.compareHash = async (objectUser) => {
+    try {
+        const project = await User.findOne({ where: { mail: objectUser.mail } });
+        if (project === null) {
+            return false
+        } else {
+            let compare = bcrypt.compareSync(objectUser.password, project.password)
+            if (compare) {
+                const payload = {
+                    data: project.id,
+                    role: project.role,
+                    iat: moment().unix(),
+                    exp: moment().add(2, 'days').unix()
+                }
+                return jwt.sign(payload, process.env.TOKEN)
+            }
+        }
+    } catch (error) {
+        res.json({
+            message: 'mail or password denegado.',
+            errors: error,
+            status: 400
+        })
+    }
+}
+module.exports.verificarToken = (req, res, next) => {
+    try {
+        jwt.verify(req.headers.token, process.env.TOKEN)
+        next()
+    } catch (error) {
+        res.json({ error: 'Acceso denegado, lo siento registrese.' })
+    }
+}
