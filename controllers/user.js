4e3d5e38284907c368184ced2e08a43ce54345d8
BMHA1 <86425408+BMHA1@users.noreply.github.com>
2021-11-08 17:12:15 +0100

    metodo eliminar usuario arreglado

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,11 @@
 
 // const { where } = require('sequelize/types')
 const { User, Appointment } = require('../models/index.js')
-const { Op } = require("sequelize")
+const { Op, DATE } = require("sequelize")
 // const { Module } = require('module')
 // const { Json } = require('sequelize/types/lib/utils')
-
+const hashing = require('../Middleware/functions')
+const { Console } = require('console')
 
 //creamos usuario
 module.exports.createUser = async (req, res) => {
@@ -12,25 +13,20 @@ module.exports.createUser = async (req, res) => {
     try {
         console.log(req.body)
         const newUser = req.body
-
+        
+        newUser.password = hashing.createHash(newUser.password)
         await User.create(newUser)
-
         res.status(200).json({ user: newUser });
 
     } catch (error) {
-        res.status(400).send({
-
+        res.status(400).json({
             message: 'No se ha podido generar un nuevo usuario.',
-            errors: error,
-            status: 400
-        });
+     });
     }
 }
 //buscamos Usuario
 module.exports.searchUser = (req, res) => {
-
     User.findByPk(req.params.id)
-
         .then((user) => {
             if (!user) res.status(200).send('El usuario no existe')
             res.status(200).json({ data: user })
@@ -55,7 +51,6 @@ module.exports.updateContent = (req, res) => {
             (error) => { res.status(200), send(error) })
 }
 
-
 //Eliminar un usuario por su ID
 module.exports.deleteUser = (req, res) => {
     console.log(res.query.id)
@@ -64,11 +59,24 @@ module.exports.deleteUser = (req, res) => {
         where: {
 
             id: {
-                [Op.in]: arr
             }
 
         }
     })
+}giy
+//login
+module.exports.loggin = async (req, res) => {
+    try {
+        let hashDescoted = await hashing.compareHash(req.body)
+        console.log(hashDescoted)
+        res.status(200).json({ hashDescoted })
+
+    } catch (error) {
+        res.json({
+            message: 'mail or password denegado.',
+            errors: error,
+            status: 400
+        })
+    }
+
 }
-
-
